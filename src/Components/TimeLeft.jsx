54c98e4a67b41d40d2d8a0fc4a96a1cf4af3af01
Omar-Walid-MD <omar.madinah49@yahoo.com>
2024-03-21 17:43:35 +0200

    Uploaded project files

---
+++ src/Components/TimeLeft.jsx
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from 'react';
+
+function TimeLeft({date,colors,prayerTimes,currentTime}) {
+
+    const prayerTime = prayerTimes[currentTime];
+    const [timeLeft,setTimeLeft] = useState(prayerTime.getTime() - Date.now());
+
+    useEffect(()=>{
+        setTimeLeft(prayerTime.getTime() - Date.now());
+    },[date]);
+
+  
+
+    function getTimeLeftString()
+    {
+        let h = "" + Math.abs(parseInt(timeLeft /(1000 * 60 * 60) % 60));
+        let m = "" + Math.abs(parseInt(timeLeft / (1000 * 60) % 60));
+        let s = "" + Math.abs(parseInt(timeLeft / 1000 % 60));
+        return `${h.length-1 ? h : "0"+h}:${m.length-1 ? m : "0"+m}:${s.length-1 ? s : "0"+s}`;
+    }
+
+    return (
+        <div className='text-capitalize text-dark fs-3 text-center fw-semibold mt-5 bg-white w-100 p-2 rounded-2 shadow'    >
+            {
+            prayerTime>=Date.now() ?
+            <>
+                Upcoming Prayer <span style={{color:`color-mix(in srgb,${colors[currentTime][1]} 80%,black)`}}>
+                    {currentTime}
+                </span> in
+            </> 
+            :
+            <>
+                <span style={{color:`color-mix(in srgb,${colors[currentTime][1]} 80%,black)`}}>
+                    {currentTime}
+                </span> Prayer Started since
+            </>
+            }: <div className='px-2'>{getTimeLeftString()}</div>
+        </div>
+    );
+}
+
+export default TimeLeft;
